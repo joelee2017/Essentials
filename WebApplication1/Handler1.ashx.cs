b4f92efcedd167cf6b0c615b5a410a706eb3f5a2
joelee2017 <as955133@gmail.com>
2018-02-21 16:34:48 +0800

    Async Get Post

---
+++ WebApplication1/Handler1.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Handler1 的摘要描述
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string Name = context.Request["Name"];
+            string Age = context.Request["Age"];
+            Thread.Sleep(2000);
+            context.Response.Write($"{Name},{Age}");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
