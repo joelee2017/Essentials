326135b91628c380b2f5021b28a33f3c60a7ef0e
joelee2017 <as955133@gmail.com>
2018-02-22 15:00:23 +0800

    public void ProcessRequest(HttpContext context)         {             string CustomerID = context.Request["CustomerID"];
                NorthwindEntities db = new NorthwindEntities();
                bool bExists = db.Customers.Any(c => c.CustomerID == CustomerID);
    
                context.Response.ContentType = "text/plain";
                context.Response.Write(bExists? "true":"false");
            }

---
+++ WebApplication1/CustomersHandler.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// CustomersHandler 的摘要描述
+    /// </summary>
+    public class CustomersHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string CustomerID = context.Request["CustomerID"];
+
+            NorthwindEntities db = new NorthwindEntities();
+            bool bExists = db.Customers.Any(c => c.CustomerID == CustomerID);
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(bExists? "true":"false"); 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
